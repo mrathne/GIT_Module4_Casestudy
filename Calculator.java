8171f3d98ef3762548b6b3379a24c95aa16af254
ubuntu <rathnayaka779@gmail.com>
2019-11-19 22:01:10 +0000

    commiting changes on substraction

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,7 @@ class Calculator implements CalculatorInterface{
    public int addition(int firtValue, int secondValue) {
    }
    public int substraction(int firtValue, int secondValue){
+	return firtValue - secondValue;
    }
    public double division(int firtValue, int secondValue){
    }
